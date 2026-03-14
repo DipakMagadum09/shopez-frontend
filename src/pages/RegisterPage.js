e347cd17fde0db68043210be585595a371e244ce
DipakMagadum09 <dipakmagadum09@gmail.com>
2026-03-14 22:15:46 +0530

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -74,7 +74,7 @@ const RegisterPage = () => {
               onChange={(e) => setForm({ ...form, userType: e.target.value })}
             >
               <option value="">User type</option>
-              <option value="admin">Admin</option>
+              //<option value="admin">Admin</option>
               <option value="customer">Customer</option>
             </select>
           </div>
